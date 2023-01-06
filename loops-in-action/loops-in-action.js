1fe26647c7373a2b9c0663736e86529c3920baf2
Wiktor Szablewski <wiktor.szablewski@outlook.com>
2023-01-06 20:21:40 +0100

    made roll dice script

--- loops-in-action/loops-in-action.js
+++ loops-in-action/loops-in-action.js
@@ -56,3 +56,39 @@ const displayData = () => {
     }
   }
 displayDataBtn.addEventListener("click", displayData);
+
+//statistic/role dice exercise
+const statisticBtn = document.querySelector('#statistics button');
+
+const rollDice = () => Math.floor(Math.random()*6) + 1;
+const deriveNumberOfDiceRolls = () => {
+  const targetRollNumber = document.querySelector("#user-roll-number").value;
+  const diceRollsList = document.querySelector("#dice-rolls");
+  diceRollsList.innerHTML = ''; //for clearing rolls on every click
+  
+  let hasRolledTargetNumber = false;
+  let numberOfRolls = 0; //while loop is incrementing this until wanted dice is outputed, its also number of total rolls outputed in final step
+
+  while(!hasRolledTargetNumber) {
+   const rolledNumber = rollDice(); //value of rolled dice is stored here
+   //double equal because input value even from number input is a string. We want to either covert that value by adding "+" in front of value or we use double equal sign comparison
+    // if(hasRolledTargetNumber == targetRollNumber) { 
+    //   hasRolledTargetNumber = true;
+    // }
+    //we can replace this if with
+    numberOfRolls++;
+
+    // outputing on page every roll until hitting the wanted dice
+    const newRollListItem = document.createElement('li');
+    newRollListItem.textContent = 'Roll '+ numberOfRolls + ': ' + rolledNumber;
+    diceRollsList.append(newRollListItem);
+    hasRolledTargetNumber = targetRollNumber == rolledNumber; // on a right sign of reasigning this expression yields true or false, leaving while loop when wanted dice in RolledNumber is equal to dice chosen by user in targetRollNumber
+  }
+  //outputing targeted dice and total number of rolls
+  const outputRolls = document.querySelector("#output-total-rolls");
+  const outputTargetDiceNumber = document.querySelector("#dice-number");
+  
+  outputTargetDiceNumber.textContent = targetRollNumber; // from input
+  outputRolls.textContent = numberOfRolls; //total number of rolls
+}
+statisticBtn.addEventListener('click', deriveNumberOfDiceRolls);
